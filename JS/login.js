67ec27912b05a496c26c4fd4746dfeeb0292714e
Adnan <adnan.hussain.136660@gmail.com>
2022-07-22 13:53:32 +0530

    Added complete Management page

--- JS/login.js
+++ JS/login.js
@@ -1,28 +1,31 @@
+let form = document.querySelector("form");
+form.addEventListener("submit", checkuser);
 
-let form=document.querySelector("form");
-form.addEventListener("submit",checkuser)
+let data = JSON.parse(localStorage.getItem("userData")) || [];
 
-let data=JSON.parse(localStorage.getItem("userData")) || []
+console.log(data);
 
-console.log(data)
-
-function checkuser(event){
+function checkuser(event) {
     event.preventDefault();
-    if(form.email.value=="" || form.password.value==""){
-        alert("Fill all fields")
-        return
+
+    if (form.email.value == "" || form.password.value == "") {
+        alert("Fill all fields");
+        return;
     }
-    let flag=false;
-    for(let i=0;i<data.length;i++){
-        if(form.email.value==data[i].email && form.password.value==data[i].password){
-            flag=true;
+    let flag = false;
+    for (let i = 0; i < data.length; i++) {
+        if (
+            form.email.value == data[i].email &&
+            form.password.value == data[i].password
+        ) {
+            flag = true;
             break;
         }
     }
-    if(flag==true){
-        alert("Login Successfull")
-        window.location.href="index.html"
-    }else{
-        alert("Wrong Credentials!")
+    if (flag == true) {
+        alert("Login Successfull");
+        window.location.href = "index.html";
+    } else {
+        alert("Wrong Credentials!");
     }
 }
