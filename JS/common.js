de52afb97343dd123d3418d746f9c92b8ff2dabb
Adnan <adnan.hussain.136660@gmail.com>
2022-07-23 12:24:57 +0530

    Added onClick event on dashboard section

--- JS/common.js
+++ JS/common.js
@@ -5,3 +5,23 @@ btn.forEach(function (ele) {
         window.location.href = "management.html";
     });
 });
+
+let changeBtn = document.querySelectorAll("#dashboard>div>button");
+
+changeBtn.forEach(function (ele) {
+    ele.addEventListener("click", function () {
+        if (ele.innerHTML == "Dashboard") {
+            // ele.style.backgroundColor="#f4f6f8"
+            document.querySelector("#dashboard>div>img").src =
+                "https://assets.website-files.com/5d55f1425cb6b7a18aa77528/60ad548421171b59d3dec8b7_dash.png";
+        }
+        if (ele.innerHTML == "Daily Performance") {
+            document.querySelector("#dashboard>div>img").src =
+                "https://assets.website-files.com/5d55f1425cb6b7a18aa77528/60ad55d0b5fc492169126b7a_graphs.png";
+        }
+        if (ele.innerHTML == "Online Listing Aggregation") {
+            document.querySelector("#dashboard>div>img").src =
+                "https://assets.website-files.com/5d55f1425cb6b7a18aa77528/60ad55d0cf2e6bf5f160cdb6_rentals.png";
+        }
+    });
+});
